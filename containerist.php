8aa691c3f2e3f2687d9aa5de908cdabe89c4c0e2
moritzz <mz@ia.net>
2018-07-22 11:02:05 +0200

    Changed timestamp routines to not collide woth Kirby's standard ->date() routines and extended output to respect datespans of a year or more.

--- containerist.php
+++ containerist.php
@@ -7,15 +7,23 @@
 function datespan($field_from, $field_to) {
   if ($field_to->isNotEmpty()) {
     $html_time_format = '%Y-%m-%dT12:00:00';
-    $from = new DateTime();
-    $to = new DateTime();
-    $from->setTimestamp($field_from);
-    $to->setTimestamp($field_to);
+    $from = new DateTime($field_from);
+    $to = new DateTime($field_to);
     $diff = $from->diff($to);
-    if (!empty($diff->m) && $diff->m > 1) {
+    // print("DateInterval is as follows:");
+    // print_r($diff);
+    $print_date_format = '%d.%m.%y';
+    if ($diff->m > 12) {
+      $print_date_format = '%Y';
+    } else if ($diff->m >= 11 && $diff->d >= 30) {
+      $print_date_format = '%Y';
+      $fromprint = strftime($print_date_format, $from->getTimestamp());
+      $fromstamp = strftime($html_time_format, $from->getTimestamp());
+      $toprint = strftime($print_date_format, $to->getTimestamp());
+      $tostamp = strftime($html_time_format, $to->getTimestamp());
+      return "<time datetime=\"$tostamp\">$toprint</time>";
+    }else if ($diff->m > 1) {
       $print_date_format = '%B %Y';
-    } else {
-      $print_date_format = '%d.%m.%y';
     }
     $fromprint = strftime($print_date_format, $from->getTimestamp());
     $fromstamp = strftime($html_time_format, $from->getTimestamp());
