08e2d450f57a51e69936e26a70d7e277d8ac5a35
Moritz Zimmer <mz@ia.net>
2018-08-24 14:09:48 +0200

    Added methods to load fully extended blueprint definition.

--- models/containerist.php
+++ models/containerist.php
@@ -5,12 +5,41 @@
 */
 class ContaineristPage extends Page {
   
+  protected $blueprint;
+
   // ===============
   // = Constructor =
   // ===============
   
   public function __construct($parent, $dirname) {
+    
     parent::__construct($parent, $dirname);
+        
+  }
+  
+  protected function setBlueprint($blueprint_name) {
+    
+    // Load and cache the blueprint
+    $this->blueprint = data::read(kirby()->get('blueprint', $blueprint_name), 'yaml');
+    // Load and add global field definitions to chached blueprint
+    ContaineristPage::extendBlueprint($this->blueprint);
+
+  }
+  
+  protected static function extendBlueprint(&$blueprint) {
+    
+    foreach ($blueprint as $key => &$item) {
+      if (is_array($item) && isset($item['extends'])) {
+        $file = kirby()->get('blueprint', 'fields/' . $item['extends']);
+        if(empty($file) || !is_file($file)) {
+          throw new Exception(l('fields.error.extended') . ' "' . $item['extends'] . '"');
+        }
+        $item = data::read($file, 'yaml');
+      } elseif (is_array($item)) {
+        ContaineristPage::extendBlueprint($item);
+      }
+    }
+    
   }
   
   // =================
